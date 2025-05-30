test(test): fix some overflow UB errors

ascend/examples/generalization_cases/test_flip.py
@@ -13,7 +13,7 @@ from test_common import TestUtils, check_ub_mem_overflow
 import triton.language.extra.ascend.libdevice as libdevice
 
 @triton.jit
-def fn_npu_1d(output_ptr, x_ptr, XB: tl.constexpr):
+def fn_npu_1d(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
     xidx = tl.arange(0, XB)
     idx = xidx
     X = tl.load(x_ptr + idx)
@@ -22,10 +22,9 @@ def fn_npu_1d(output_ptr, x_ptr, XB: tl.constexpr):
     tl.store(output_ptr + oidx, ret)
 
 @triton.jit
-def fn_npu_2d(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr):
-    xoffs = tl.program_id(0) * XB
-    xidx = tl.arange(0, XB) + xoffs
-    yidx = tl.arange(0, YB)
+def fn_npu_2d(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
+    xidx = tl.arange(0, XB) + tl.program_id(0) * XB
+    yidx = tl.arange(0, YB) + tl.program_id(1) * YB
     idx = xidx[:, None] * YB + yidx[None, :]
     X = tl.load(x_ptr + idx)
     ret = libdevice.flip(X, 1)
@@ -34,9 +33,9 @@ def fn_npu_2d(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr):
 
 @triton.jit
 def fn_npu_3d(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
-    xidx = tl.arange(0, XB)
-    yidx = tl.arange(0, YB)
-    zidx = tl.arange(0, ZB)
+    xidx = tl.arange(0, XB) + tl.program_id(0) * XB
+    yidx = tl.arange(0, YB) + tl.program_id(1) * YB
+    zidx = tl.arange(0, ZB) + tl.program_id(2) * ZB
     idx = xidx[:, None, None] * YB * ZB + yidx[None, :, None] * ZB + zidx[None, None, :]
     X = tl.load(x_ptr + idx)
     ret = libdevice.flip(X, 2)
@@ -69,23 +68,34 @@ def test_flip(shape, dtype):
     else:
         x = torch.randint(low=0, high=128, size=shape, dtype=data_dtype).npu()
 
-    # torch_npu䷾M弾T¯弾L~Auint32漾Z~Dflip
     torch_input = x if x.dtype != torch.uint32 else x.to(torch.float32)
     torch_res = torch.flip(torch_input, dims=(-1,))
     triton_res = torch.empty(shape, dtype=data_dtype).npu()
-    if len(shape) == 1:
-        fn_npu_1d[1, 1, 1](triton_res, x, shape[0])
-    elif len(shape) == 2:
-        shape0 = shape[0]
+    
+    grid = [1, 1, 1]
+    shape0 = shape[0]
+    shape1 = 1
+    shape2 = 1
+    if len(shape) == 2:
         shape1 = shape[1]
-        if x.numel() * x.element_size() >= 8192:
-            grid = (shape0, 1, 1)
-            shape0 = 1
-        else:
-            grid = (1, 1, 1)
-        fn_npu_2d[grid](triton_res, x, shape0, shape1)
+    if len(shape) == 3:
+        shape1 = shape[1]
+        shape2 = shape[2]
+    values = [shape0, shape1, shape2]
+    if dtype == 'bool':
+        if x.numel() * x.element_size() >= 512 and len(shape) > 1:
+            grid = [shape0, 1, 1]
+            values[0] = 1    
+    else:
+        if x.numel() * x.element_size() >= 8192 and len(shape) > 1:
+            grid = [shape0, 1, 1]
+            values[0] = 1   
+    if len(shape) == 1:
+        fn_npu_1d[grid](triton_res, x, values[0], values[1], values[2])
+    elif len(shape) == 2:
+        fn_npu_2d[grid](triton_res, x, values[0], values[1], values[2])
     elif len(shape) == 3:
-        fn_npu_3d[1, 1, 1](triton_res, x, shape[0], shape[1], shape[2])
+        fn_npu_3d[grid](triton_res, x, values[0], values[1], values[2])
 
     triton_res = triton_res if triton_res.dtype != torch.uint32 else triton_res.to(torch.float32)
     cmp_dtype = dtype if dtype != 'uint32' else 'float32'