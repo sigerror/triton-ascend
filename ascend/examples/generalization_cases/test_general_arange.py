test(test): fix testcase ub overflow error and matmul increased accuracy comparison of cpu-torch_npu-triton

ascend/examples/generalization_cases/test_general_arange.py
@@ -1,10 +1,10 @@
+import math
 import triton
 import triton.language as tl
 import torch
 import pytest
 import test_common
 from test_common import TestUtils
-import math
 
 def torch_pointwise(length):
     res = (torch.arange(0,length) / 2.7) * torch.arange(0, length)
@@ -20,7 +20,7 @@ def triton_arange(out_ptr0, length:tl.constexpr, numel: tl.constexpr):
     tl.store(out_ptr0 + idx, b, mask)
 
 @pytest.mark.parametrize('shape', TestUtils.test_shape1d)
-@pytest.mark.parametrize('dtype', ['int32', 'int16', 'int8'])
+@pytest.mark.parametrize('dtype', ['int32', 'int16', 'int8', 'int64'])
 def test_case(dtype, shape):
     x0 = test_common.generate_tensor(shape, dtype).npu()
     x1 = test_common.generate_tensor(shape, dtype).npu()

ascend/examples/generalization_cases/test_general_gather.py
@@ -1,11 +1,13 @@
+import math
+import numpy as np
 import torch
 import torch_npu
 import triton
 import triton.language as tl
 import triton.language.extra.ascend.libdevice as libdevice
-import numpy as np
 import test_common
 import pytest
+from test_common import TestUtils, check_ub_mem_overflow, get_dtype_size
 
 # @pytest.mark.skip(reason="waiting for the compiler to support.")
 @pytest.mark.parametrize("src_shape, indices_shape, axis", [
@@ -50,6 +52,12 @@ def test_gather(src_shape, indices_shape, axis):
     DEV = "npu"
     src = torch.randn(src_shape, device=DEV)
     indices = torch.randint(0, src.shape[axis], indices_shape, device=DEV)
+
+    dtype_size = get_dtype_size('int32')
+    if dtype_size * math.prod(src.shape) >= (TestUtils.ub_size / 8):
+        print(f"dtype:int32 shape:{src.shape} mem overflow")
+        return
+
     ref = torch.gather(src, axis, indices)
     result = triton_gather(src, axis, indices)
     torch.testing.assert_close(result, ref, rtol=0, atol=0)

ascend/examples/generalization_cases/test_matmul.py
@@ -1,11 +1,14 @@
+import logging
+import pytest
 import torch
 import torch_npu
 import triton
 import triton.language as tl
-import pytest
-import logging
+
+import acc_util
 import test_common
 from test_common import TestUtils, avoid_not_support, get_dtype_size
+
 
 @triton.jit
 def matmul_kernel(
@@ -48,6 +51,7 @@ def matmul_kernel(
     c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
     tl.store(c_ptrs, c, mask=c_mask)
 
+
 @avoid_not_support('matmul')
 @pytest.mark.parametrize('shape', TestUtils.test_shape2d)
 @pytest.mark.parametrize('dtype', TestUtils.dtype_list)
@@ -57,19 +61,35 @@ def test_matmul(shape, dtype):
     # bisheng not support yet
     if M % 16 != 0 or N % 16 != 0 or get_dtype_size(dtype) * K % 32 != 0:
         return
-
-    BLOCK_M, BLOCK_N, BLOCK_K = min(max(M, 16), 32), min(max(N, 16), 32), min(max(K, 16), 32)
+    kalign = 32 / get_dtype_size(dtype)  # 32byte/Dtype_bytes
+    BLOCK_M, BLOCK_N, BLOCK_K = min(max(M, 16), 32), min(max(N, 16), 32), min(max(K, kalign), 32)
     a = test_common.generate_tensor((M, K), dtype)
     b = test_common.generate_tensor((K, N), dtype)
-    torch_res = torch.mm(a, b).npu()
+
     triton_res = torch.zeros((M, N), dtype=eval('torch.' + dtype)).npu()
-    grid = (triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N), )
+    grid = (triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N),)
     matmul_kernel[grid](a.npu(), b.npu(), triton_res, M, N, K, tl.float32,
                         a.stride(0), a.stride(1), b.stride(0), b.stride(1),
                         triton_res.stride(0), triton_res.stride(1),
                         BLOCK_M, BLOCK_N, BLOCK_K)
-    logging.debug(f'triton:\n{triton_res}\ntorch:\n{torch_res}')
-    test_common.validate_cmp(dtype, triton_res, torch_res)
+
+    a_gold = a.to(torch.float32)
+    b_gold = b.to(torch.float32)
+    cpu_res = torch.mm(a_gold, b_gold)
+
+    a_npu = a.npu()
+    b_npu = b.npu()
+    torch_res = torch.mm(a_npu, b_npu)
+
+    try:
+        print("starting compare of cpu vs triton:")
+        acc_util.assert_close(cpu_res, triton_res)
+    except Exception as e:
+        print(e)
+        print("starting compare of cpu vs triton vs torch_npu:")
+        acc_util.benchmark_compare_close(cpu_res, triton_res, torch_res)
+    print("PASSED")
+
 
 if __name__ == "__main__":
     test_matmul((16, 32), 'float32')

ascend/examples/generalization_cases/test_max.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
+import math
 import triton
 import triton.language as tl
 import torch
 import torch_npu
 import pytest
 import test_common
-from test_common import TestUtils, check_ub_mem_overflow
+from test_common import TestUtils, check_ub_mem_overflow, get_dtype_size
 
 # <<<<<<< test_max_1d
 def torch_max(x0, dim, keepdim):
@@ -54,8 +55,15 @@ def triton_max_2d(in_ptr0, out_ptr0, dim : tl.constexpr, M : tl.constexpr, N : t
 @pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'bfloat16', 'float32'])
 @pytest.mark.parametrize('dim', [0, 1])
 def test_max_2d(dtype, shape, dim):
-    if check_ub_mem_overflow(dtype, shape):
+    dtype_size = get_dtype_size(dtype)
+    if dtype == 'int8':
+        if dtype_size * math.prod(shape) >= (TestUtils.ub_size / 20):
+            print(f"dtype:{dtype} shape:{shape} mem overflow")
+            return
+    elif dtype_size * math.prod(shape) >= (TestUtils.ub_size / 5):
+        print(f"dtype:{dtype} shape:{shape} mem overflow")
         return
+
     shapex, shapey = shape
     x0 = test_common.generate_tensor(shape, dtype).npu()
     triton_res = torch.empty([shape[1-dim], ], dtype=eval("torch." + dtype)).npu()

ascend/examples/generalization_cases/test_sum.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
+import math
 import triton
 import triton.language as tl
 import torch
 import pytest
 import test_common
 from test_common import TestUtils
-import math
 def torch_sum(x1,dim):
     if x1.dtype == torch.float16 or x1.dtype == torch.bfloat16:
         res = torch.sum(x1.to(torch.float32), dim, keepdim=False).to(x1.dtype)

ascend/examples/generalization_cases/test_xorsum.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
+import math
 import triton
 import triton.language as tl
 import torch
@@ -7,7 +8,7 @@ import torch_npu
 import pytest
 import test_common
 import functools
-from test_common import TestUtils, check_ub_mem_overflow
+from test_common import TestUtils, check_ub_mem_overflow, get_dtype_size
 
 # <<<<<<< test_xorsum_1d
 def torch_xorsum(tensor, dim=None, keepdim=False):
@@ -66,8 +67,12 @@ def triton_xorsum_2d(in_ptr0, out_ptr0, dim : tl.constexpr, M : tl.constexpr, N 
 @pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64'])
 @pytest.mark.parametrize('dim', [0, 1])
 def test_xorsum_2d(dtype, shape, dim):
-    if check_ub_mem_overflow(dtype, shape):
-        return
+    dtype_size = get_dtype_size(dtype)
+    if dtype in ['int8', 'int16', 'int32', 'int64']:
+        if dtype_size * math.prod(shape) >= (TestUtils.ub_size / 3):
+            print(f"dtype:{dtype} shape:{shape} mem overflow")
+            return
+
     shapex, shapey = shape
     x0 = test_common.generate_tensor(shape, dtype).npu()
     triton_res = torch.empty([shape[1-dim], ], dtype=eval("torch." + dtype)).npu()