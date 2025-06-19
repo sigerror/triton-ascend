fix(secure): Add security compilation options PIE and fix argmax/broadcast cases

ascend/examples/generalization_cases/test_argmax.py
@@ -1,73 +1,90 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
-import triton
-import triton.language as tl
+import logging
+import math
+import pytest
 import torch
 import torch_npu
-import pytest
+import triton
+import triton.language as tl
+
 import test_common
-from test_common import TestUtils, check_ub_mem_overflow
+from test_common import TestUtils, check_ub_mem_overflow, get_dtype_size
+
+logger = logging.getLogger(__name__)
+
 
 # <<<<<<< test_argmax_1d
 def torch_argmax(x0, dim, keepdim):
     x0 = x0 if x0.device == "cpu" else x0.cpu()
     return torch.argmax(x0, dim=dim, keepdim=keepdim).npu()
 
+
 @triton.jit
-def triton_argmax_1d(in_ptr0, out_ptr1, xnumel, XBLOCK : tl.constexpr):
+def triton_argmax_1d(in_ptr0, out_ptr1, xnumel, XBLOCK: tl.constexpr):
     xoffset = tl.program_id(0) + tl.arange(0, XBLOCK)
     tmp0 = tl.load(in_ptr0 + xoffset, None)
     tmp4 = tl.argmax(tmp0, 0)
     tl.store(out_ptr1, tmp4, None)
 
+
 @pytest.mark.parametrize('shape', TestUtils.test_shape1d)
 @pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'bfloat16', 'float32'])
 def test_argmax_1d(dtype, shape):
-    if check_ub_mem_overflow(dtype, shape):
+    dtype_size = get_dtype_size(dtype)
+    if dtype_size * math.prod(shape) >= (TestUtils.ub_size / 3):
+        logger.warning(f"dtype:{dtype} shape:{shape} mem overflow")
         return
     x0 = test_common.generate_tensor(shape, dtype).npu()
     triton_res = torch.empty(1, dtype=torch.int32).npu()
     numel = shape[0]
-    triton_argmax_1d[1,1,1](x0, triton_res, numel, numel)
+    triton_argmax_1d[1, 1, 1](x0, triton_res, numel, numel)
     torch_res = torch_argmax(x0, dim=0, keepdim=True)
     test_common.validate_cmp("int32", triton_res, torch_res)
+
 
 # >>>>>>> test_argmax_1d
 
 # <<<<<<< test_argmax_2d
 @triton.jit
 def triton_argmax_2d(in_ptr0, out_ptr0, dim : tl.constexpr, M : tl.constexpr, N : tl.constexpr, MNUMEL: tl.constexpr, NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0,MNUMEL)
-    nblk_idx = tl.arange(0,NNUMEL)
+    mblk_idx = tl.arange(0, MNUMEL)
+    nblk_idx = tl.arange(0, NNUMEL)
     mmask = mblk_idx < M
     nmask = nblk_idx < N
-    mask = (mmask[:,None]) & (nmask[None,:])
-    idx = mblk_idx[:,None] * N + nblk_idx[None,:]
-    x = tl.load(in_ptr0 + idx, mask = mask, other = -float('inf'))
+    mask = (mmask[:, None]) & (nmask[None, :])
+    idx = mblk_idx[:, None] * N + nblk_idx[None, :]
+    x = tl.load(in_ptr0 + idx, mask=mask, other=-float('inf'))
     tmp4 = tl.argmax(x, dim)
     if dim == 0:
-        tl.store(out_ptr0 + tl.arange(0,N), tmp4, None)
+        tl.store(out_ptr0 + tl.arange(0, N), tmp4, None)
     else:
-        tl.store(out_ptr0 + tl.arange(0,M), tmp4, None)
+        tl.store(out_ptr0 + tl.arange(0, M), tmp4, None)
+
 
 @pytest.mark.parametrize('shape', TestUtils.test_shape2d)
 @pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'bfloat16', 'float32'])
 @pytest.mark.parametrize('dim', [0, 1])
 def test_argmax_2d(dtype, shape, dim):
-    if check_ub_mem_overflow(dtype, shape):
+    dtype_size = get_dtype_size(dtype)
+    if dtype_size * math.prod(shape) >= (TestUtils.ub_size / 3):
+        logger.warning(f"dtype:{dtype} shape:{shape} mem overflow")
         return
     shapex, shapey = shape
     x0 = test_common.generate_tensor(shape, dtype).npu()
-    triton_res = torch.empty([shape[1-dim], ], dtype=torch.int32).npu()
-    triton_argmax_2d[1,1,1](x0, triton_res, dim, shapex, shapey, shapex, shapey)
+    triton_res = torch.empty([shape[1 - dim], ], dtype=torch.int32).npu()
+    triton_argmax_2d[1, 1, 1](x0, triton_res, dim, shapex, shapey, shapex, shapey)
     torch_res = torch_argmax(x0, dim=dim, keepdim=False)
     test_common.validate_cmp("int32", triton_res, torch_res)
+
 
 # >>>>>>> test_argmax_2d
 
 # <<<<<<< test_argmax_3d
 def torch_argmax_3d(x0, no_reduce_dim):
     x0 = x0 if x0.device == "cpu" else x0.cpu()
+    if x0.dtype in (torch.int8, torch.int16, torch.int32):
+        x0 = x0.to(torch.int64)
     if no_reduce_dim == 0:
         return torch.argmax(torch.max(x0, 1)[0], 1).npu()
     elif no_reduce_dim == 1:
@@ -77,16 +94,16 @@ def torch_argmax_3d(x0, no_reduce_dim):
     else:
         assert False, f"no reduce dim not right, no_reduce_dim = {no_reduce_dim}"
 
+
 @triton.jit
 def triton_argmax_3d_0_1(in_ptr, out_ptr,
-    xnumel:tl.constexpr, ynumel:tl.constexpr, znumel:tl.constexpr,
-    XB:tl.constexpr, YB:tl.constexpr, ZB:tl.constexpr):
+                         xnumel: tl.constexpr, ynumel: tl.constexpr, znumel: tl.constexpr,
+                         XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
+    xidx = tl.arange(0, XB)
+    yidx = tl.arange(0, YB)
+    zidx = tl.arange(0, ZB)
 
-    xidx = tl.arange(0,XB)
-    yidx = tl.arange(0,YB)
-    zidx = tl.arange(0,ZB)
-
-    idx = xidx[:,None,None]*ynumel*znumel + yidx[None,:,None]*znumel + zidx[None,None,:]
+    idx = xidx[:, None, None] * ynumel * znumel + yidx[None, :, None] * znumel + zidx[None, None, :]
 
     x = tl.load(in_ptr + idx)
 
@@ -95,16 +112,16 @@ def triton_argmax_3d_0_1(in_ptr, out_ptr,
     oidx = zidx
     tl.store(out_ptr + oidx, ret)
 
+
 @triton.jit
 def triton_argmax_3d_0_2(in_ptr, out_ptr,
-    xnumel:tl.constexpr, ynumel:tl.constexpr, znumel:tl.constexpr,
-    XB:tl.constexpr, YB:tl.constexpr, ZB:tl.constexpr):
+                         xnumel: tl.constexpr, ynumel: tl.constexpr, znumel: tl.constexpr,
+                         XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
+    xidx = tl.arange(0, XB)
+    yidx = tl.arange(0, YB)
+    zidx = tl.arange(0, ZB)
 
-    xidx = tl.arange(0,XB)
-    yidx = tl.arange(0,YB)
-    zidx = tl.arange(0,ZB)
-
-    idx = xidx[:,None,None]*ynumel*znumel + yidx[None,:,None]*znumel + zidx[None,None,:]
+    idx = xidx[:, None, None] * ynumel * znumel + yidx[None, :, None] * znumel + zidx[None, None, :]
 
     x = tl.load(in_ptr + idx)
 
@@ -113,16 +130,16 @@ def triton_argmax_3d_0_2(in_ptr, out_ptr,
     oidx = yidx
     tl.store(out_ptr + oidx, ret)
 
+
 @triton.jit
 def triton_argmax_3d_1_2(in_ptr, out_ptr,
-    xnumel:tl.constexpr, ynumel:tl.constexpr, znumel:tl.constexpr,
-    XB:tl.constexpr, YB:tl.constexpr, ZB:tl.constexpr):
+                         xnumel: tl.constexpr, ynumel: tl.constexpr, znumel: tl.constexpr,
+                         XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
+    xidx = tl.arange(0, XB)
+    yidx = tl.arange(0, YB)
+    zidx = tl.arange(0, ZB)
 
-    xidx = tl.arange(0,XB)
-    yidx = tl.arange(0,YB)
-    zidx = tl.arange(0,ZB)
-
-    idx = xidx[:,None,None]*ynumel*znumel + yidx[None,:,None]*znumel + zidx[None,None,:]
+    idx = xidx[:, None, None] * ynumel * znumel + yidx[None, :, None] * znumel + zidx[None, None, :]
 
     x = tl.load(in_ptr + idx)
 
@@ -131,6 +148,7 @@ def triton_argmax_3d_1_2(in_ptr, out_ptr,
     oidx = xidx
     tl.store(out_ptr + oidx, ret)
 
+
 def triton_argmax_3d(in_ptr, out_ptr, xnumel, ynumel, znumel, XB, YB, ZB, no_reduce_dim):
     if no_reduce_dim == 0:
         triton_argmax_3d_1_2[1, 1, 1](in_ptr, out_ptr, xnumel, ynumel, znumel, XB, YB, ZB)
@@ -138,6 +156,7 @@ def triton_argmax_3d(in_ptr, out_ptr, xnumel, ynumel, znumel, XB, YB, ZB, no_red
         triton_argmax_3d_0_2[1, 1, 1](in_ptr, out_ptr, xnumel, ynumel, znumel, XB, YB, ZB)
     elif no_reduce_dim == 2:
         triton_argmax_3d_0_1[1, 1, 1](in_ptr, out_ptr, xnumel, ynumel, znumel, XB, YB, ZB)
+
 
 @pytest.mark.parametrize('shape', TestUtils.test_shape3d)
 @pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'bfloat16', 'float32'])

ascend/examples/generalization_cases/test_broadcast.py
@@ -76,7 +76,7 @@ def test_broadcast_to_dim0(shape, dtype):
     x0 = test_common.generate_tensor(shape=(1, M, N), dtype=dtype).npu()
     ans = x0.repeat(L, 1, 1)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim0[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim0[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)
 
 
@@ -100,7 +100,7 @@ def test_broadcast_to_dim1(shape, dtype):
     x0 = test_common.generate_tensor(shape=(L, 1, N), dtype=dtype).npu()
     ans = x0.repeat(1, M, 1)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim1[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim1[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)
 
 
@@ -124,7 +124,7 @@ def test_broadcast_to_dim2(shape, dtype):
     x0 = test_common.generate_tensor(shape=(L, M, 1), dtype=dtype).npu()
     ans = x0.repeat(1, 1, N)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim2[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim2[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)
 
 
@@ -148,7 +148,7 @@ def test_broadcast_to_dim01(shape, dtype):
     x0 = test_common.generate_tensor(shape=(1, 1, N), dtype=dtype).npu()
     ans = x0.repeat(L, M, 1)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim01[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim01[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)
 
 
@@ -172,7 +172,7 @@ def test_broadcast_to_dim02(shape, dtype):
     x0 = test_common.generate_tensor(shape=(1, M, 1), dtype=dtype).npu()
     ans = x0.repeat(L, 1, N)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim02[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim02[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)
 
 
@@ -196,5 +196,5 @@ def test_broadcast_to_dim12(shape, dtype):
     x0 = test_common.generate_tensor(shape=(L, 1, 1), dtype=dtype).npu()
     ans = x0.repeat(1, M, N)
     output = torch.zeros((L, M, N), dtype=eval('torch.' + dtype)).npu()
-    triton_broadcast_to_dim12[1, 1, 1](x0, output, L, M, N, debug=True)
+    triton_broadcast_to_dim12[1, 1, 1](x0, output, L, M, N)
     test_common.validate_cmp(dtype, output, ans)