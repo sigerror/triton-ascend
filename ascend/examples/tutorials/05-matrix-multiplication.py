fd05fb251e23e6128d0768a47fe828f1ad46e692
ShiJingchang <shijingchang@huawei.com>
2025-06-26 12:32:57 +0000

    chore(tutorial): refactor matmul tutorial to be similar to the community

--- ascend/examples/tutorials/05-matrix-multiplication.py
+++ ascend/examples/tutorials/05-matrix-multiplication.py
@@ -2,108 +2,193 @@
 Matrix Multiplication
 ===============
 """
+
 import triton
 import triton.language as tl
 import torch
 import torch_npu
-import pytest
 
-def torch_dot_None(x0, x1):
-    res = torch.matmul(x0, x1)
-    return res
+DEV = "npu"
+activation = "leaky_relu"
 
-def generate_tensor(shape, dtype):
-    if dtype == 'float32' or dtype == 'float16' or dtype == 'bfloat16':
-        return torch.randn(size=shape, dtype=eval('torch.' + dtype))
-    elif dtype == 'int32' or dtype == 'int64' or dtype == 'int16':
-        return torch.randint(low=0, high=2000, size=shape, dtype=eval('torch.' + dtype))
-    elif dtype == 'int8':
-        return torch.randint(low=0, high=127, size=shape, dtype=eval('torch.' + dtype))
-    elif dtype == 'bool':
-        return torch.randint(low=0, high=2, size=shape).bool()
-    else:
-        raise ValueError('Invalid parameter \"dtype\" is found : {}'.format(dtype))
 
-def validate_cmp(dtype, y_cal, y_ref):
-    y_cal = y_cal.npu()
-    y_ref = y_ref.npu()
-    if dtype == 'float16':
-        torch.testing.assert_close(y_ref, y_cal, rtol=1e-03, atol=1e-03, equal_nan=True)
-    elif dtype == 'bfloat16':
-        torch.testing.assert_close(y_ref.to(torch.float32), y_cal.to(torch.float32), rtol=1e-03, atol=1e-03, equal_nan=True)
-    elif dtype == 'float32':
-        torch.testing.assert_close(y_ref, y_cal, rtol=1e-04, atol=1e-04, equal_nan=True)
-    elif dtype == 'int32' or dtype == 'int64'or dtype == 'int16' or dtype == 'int8':
-        assert torch.equal(y_cal, y_ref)
-    elif dtype == 'bool':
-        assert torch.equal(y_cal, y_ref)
-    else:
-        raise ValueError('Invalid parameter \"dtype\" is found : {}'.format(dtype))
+def get_autotune_config():
+    return [
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 128}),
+        triton.Config({"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 64, "BLOCK_SIZE_K": 64}),
+    ]
 
-def get_torch_typename(dtype):
-    if dtype == 'float32':
-        tyname = torch.float32
-    elif dtype == 'int32':
-        tyname = torch.int32
-    elif dtype == 'int64':
-        tyname = torch.int64
-    elif dtype == 'float16':
-        tyname = torch.float16
-    elif dtype == 'int16':
-        tyname = torch.int16
-    elif dtype == 'int8':
-        tyname = torch.int8
-    elif dtype == 'bool':
-        tyname = torch.bool
-    elif dtype == 'bfloat16':
-        tyname = torch.bfloat16
-    else:
-        raise ValueError('Invalid parameter \"dtype\" is found : {}'.format(dtype))
-    return tyname
 
+@triton.autotune(
+    configs=get_autotune_config(),
+    key=["M", "N", "K"],
+)
 @triton.jit
-def triton_dot_2_None(output_ptr, x_ptr, y_ptr, z_ptr,A : tl.constexpr,B : tl.constexpr,C : tl.constexpr,D : tl.constexpr):
-    aidx=tl.arange(0,A)
-    bidx=tl.arange(0,B)
-    cidx=tl.arange(0,C)
-    didx=tl.arange(0,D)
-    accumulator = tl.zeros((B, D), dtype=tl.float32)
-    Xidx=bidx[:,None]*C+cidx[None,:]
-    Yidx=cidx[:,None]*D+didx[None,:]
-    Zidx=bidx[:,None]*D+didx[None,:]
-    X = tl.load(x_ptr+Xidx)
-    Y = tl.load(y_ptr+Yidx)
-    Z = tl.load(z_ptr+Zidx)
-    ret = tl.dot(X, Y)
-    oidx=bidx[:,None]*D+didx[None,:]
-    tl.store(output_ptr+oidx,ret)
-    
-testlist = [
-    (3, 16, 16, 16),
-]
-typelist = ['float16',]
+def matmul_kernel(
+    # Pointers to matrices
+    a_ptr,
+    b_ptr,
+    c_ptr,
+    # Matrix dimensions
+    M,
+    N,
+    K,
+    # The stride variables represent how much to increase the ptr by when moving by 1
+    # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+    # by to get the element one row down (A has M rows).
+    stride_am,
+    stride_ak,  #
+    stride_bk,
+    stride_bn,  #
+    stride_cm,
+    stride_cn,
+    # Meta-parameters
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,  #
+    ACTIVATION: tl.constexpr,  #
+):
+    """Kernel for computing the matmul C = A x B.
+    A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+    """
+    GROUP_SIZE_M: tl.constexpr = 1
+    # -----------------------------------------------------------
+    # Map program ids `pid` to the block of C it should compute.
+    # This is done in a grouped ordering to promote L2 data reuse.
+    # See above `L2 Cache Optimizations` section for details.
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
 
-@pytest.mark.parametrize('A, B, C, D',testlist)
-@pytest.mark.parametrize('sigtype',typelist)
-def test_dot_2_None(sigtype, A, B, C, D):
-    dtype = get_torch_typename(sigtype)
-    x0 = generate_tensor(shape = (B, C),dtype = sigtype).npu()
-    x1 = generate_tensor(shape = (C, D),dtype = sigtype).npu()
-    if 'int' in sigtype:
-        x2 = generate_tensor(shape = (B, D),dtype = 'int32').npu()
-        ans = torch_dot_None(x0.to(torch.float32), x1.to(torch.float32)).to(dtype)
-    else:
-        x2 = generate_tensor(shape = (B, D),dtype = 'float32').npu()
-        ans = torch_dot_None(x0, x1)
-    output = torch.zeros((B, D), dtype = dtype).npu()
-    triton_dot_2_None[1,1,1](output, x0, x1, x2, A, B, C, D, debug = True)
-    validate_cmp(sigtype,output,ans)
+    # ----------------------------------------------------------
+    # Create pointers for the first blocks of A and B.
+    # We will advance this pointer as we move in the K direction
+    # and accumulate
+    # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+    # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+    # See above `Pointer Arithmetic` section for details
+    offs_am = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_bn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    msk_m = offs_am < M
+    msk_n = offs_bn < N
+    # -----------------------------------------------------------
+    # Iterate to compute a block of the C matrix.
+    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+    # of fp32 values for higher accuracy.
+    # `accumulator` will be converted back to fp16 after the loop.
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        # Load the next block of A and B, generate a mask by checking the K dimension.
+        # If it is out of bounds, set it to 0.
+        a = tl.load(
+            a_ptrs,
+            mask=msk_m[:, None] and (offs_k[None, :] < K - k * BLOCK_SIZE_K),
+            other=0.0,
+        )
+        b = tl.load(
+            b_ptrs,
+            mask=msk_n[None, :] and (offs_k[:, None] < K - k * BLOCK_SIZE_K),
+            other=0.0,
+        )
+        # We accumulate along the K dimension.
+        accumulator = tl.dot(a, b, accumulator)
+        # Advance the ptrs to the next K block.
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+    # You can fuse arbitrary activation functions here
+    # while the accumulator is still in FP32!
+    if ACTIVATION == "leaky_relu":
+        SUB_BLK_M: tl.constexpr = BLOCK_SIZE_M // 2
+        for s in range(0, 2):
+            vec_sub_blk = tl.subview(
+                accumulator, (s * SUB_BLK_M, 0), (SUB_BLK_M, BLOCK_SIZE_N), (1, 1)
+            )
+            vec_sub_blk = leaky_relu(vec_sub_blk)
+            accumulator = tl.insert(
+                accumulator,
+                vec_sub_blk,
+                (s * SUB_BLK_M, 0),
+                (SUB_BLK_M, BLOCK_SIZE_N),
+                (1, 1),
+            )
+    c = accumulator.to(tl.float16)
 
-if __name__=='__main__':
-    for A,B,C,D in testlist:
-        for sigtype in typelist:
-            try:
-                test_dot_2_None(sigtype, A, B, C, D)
-                print(f"Test matmul with dtype={sigtype}, shape=({A},{B},{C},{D}) PASSED!")
-            except AssertionError as e:
-                print(f"Test matmul with dtype={sigtype}, shape=({A},{B},{C},{D}) FAILED!")
+    # -----------------------------------------------------------
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
+
+
+# We can fuse `leaky_relu` by providing it as an `ACTIVATION` meta-parameter in `matmul_kernel`.
+@triton.jit
+def leaky_relu(x):
+    return tl.where(x >= 0, x, 0.01 * x)
+
+
+def torch_matmul(a, b, activation=""):
+    c = torch.matmul(a, b)
+    if activation == "leaky_relu":
+        c = torch.where(c >= 0, c, 0.01 * c)
+    return c
+
+
+# %%
+# We can now create a convenience wrapper function that only takes two input tensors,
+# and (1) checks any shape constraint; (2) allocates the output; (3) launches the above kernel.
+
+
+def matmul(a, b, activation=""):
+    # Check constraints.
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    M, K = a.shape
+    K, N = b.shape
+    # Allocates output.
+    c = torch.empty((M, N), device=a.device, dtype=torch.float16)
+    # 1D launch kernel where each block gets its own program.
+    grid = lambda META: (
+        triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"]),
+    )
+    matmul_kernel[grid](
+        a,
+        b,
+        c,  #
+        M,
+        N,
+        K,  #
+        a.stride(0),
+        a.stride(1),  #
+        b.stride(0),
+        b.stride(1),  #
+        c.stride(0),
+        c.stride(1),  #
+        ACTIVATION=activation,  #
+    )
+    return c
+
+
+# %%
+# Unit Test
+# ---------
+#
+# We can test our custom matrix multiplication operation against a native torch implementation (i.e., cuBLAS).
+torch.npu.set_device(1)
+torch.manual_seed(0)
+a = torch.randn((512, 512), device=DEV, dtype=torch.float16)
+b = torch.randn((512, 512), device=DEV, dtype=torch.float16)
+triton_output = matmul(a, b, activation)
+torch_output = torch_matmul(a, b, activation)
+print(f"triton_output_with_fp16_inputs={triton_output}")
+print(f"torch_output_with_fp16_inputs={torch_output}")
