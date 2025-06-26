aa1c6ae2f1f61e2880f8ffdd388b44ffe953d3a4
刘风昇 <liufengsheng2@huawei.com>
2025-06-26 12:29:41 +0000

    test(op): add some test case for swizzle2d

--- ascend/examples/generalization_cases/test_swizzle2d.py
+++ ascend/examples/generalization_cases/test_swizzle2d.py
@@ -1,48 +1,55 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
 
+import random
 import triton
 import triton.language as tl
 import torch
 import torch_npu
 import pytest
 import test_common
+from test_common import TestUtils
+
+
+def swizzle2d(size_i, size_j, size_g):
+    i = torch.arange(0, size_i)[:, None]
+    j = torch.arange(0, size_j)[None, :]
+    ij = i * size_j + j
+    size_gj = size_g * size_j
+    group_id = ij // size_gj
+    off_i = group_id * size_g
+    size_g = torch.min(size_i - off_i, torch.tensor(size_g).expand_as(off_i))
+    ij = ij % size_gj
+    new_i = off_i + ij % size_g
+    new_j = ij // size_g
+    ret = new_i * size_i + new_j
+    return ret
 
 
 @triton.jit
-def fn_npu_(output_ptr, x_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
-    xidx = tl.arange(0, XB)
-    yidx = tl.arange(0, YB)
-    zidx = tl.arange(0, ZB)
+def fn_npu_(out0, out1, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr):
+    i = tl.arange(0, XB)[:, None]
+    j = tl.arange(0, YB)[None, :]
+    ij = i * YB + j
+    xx, yy = tl.swizzle2d(i, j, size_i=XB, size_j=YB, size_g=ZB)
 
-    i = tl.arange(0, 4)
-    j = tl.arange(0, 4)
-    xx, yy = tl.swizzle2d(i, j, size_i=4, size_j=4, size_g=2)
+    ptr = tl.load(out0)
+    xx = tl.cast(xx, dtype=ptr.dtype)
+    yy = tl.cast(yy, dtype=ptr.dtype)
+    tl.store(out0 + ij, xx)
+    tl.store(out1 + ij, yy)
 
-    tl.store(output_ptr + tl.arange(0, 4), xx)
-    tl.store(x_ptr + tl.arange(0, 4), yy)
 
+@pytest.mark.parametrize('shape', TestUtils.test_shape2d)
+@pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64'])
+def test_swizzle2d(shape, dtype):
+    if (shape[0] > 255) or (shape[1] > 255):
+        return
+    size_g = random.randint(1, min(shape[0], shape[1]))
+    ans = swizzle2d(shape[0], shape[1], size_g).to(eval('torch.' + dtype)).npu()
 
-@pytest.mark.parametrize('param_list',
-                         [
-                             ['int8', (2, 256, 16), 1, 2, 256, 16],
-                             ['int16', (2, 256, 16), 1, 2, 256, 16],
-                             ['int32', (2, 256, 16), 1, 2, 256, 16],
-                             ['int64', (2, 256, 16), 1, 2, 256, 16],
-                             ['float16', (2, 256, 16), 1, 2, 256, 16],
-                             ['float32', (2, 256, 16), 1, 2, 256, 16],
-                             ['bfloat16', (2, 256, 16), 1, 2, 256, 16],
-                         ]
-                         )
-
-def test_swizzle2d(param_list):
-    dtype, shape, ncore, XB, YB, ZB = param_list
-    x = test_common.generate_tensor((4,), dtype).npu()
-    a = torch.tensor([[0, 2, 1, 3],
-                      [4, 6, 5, 7],
-                      [8, 10, 9, 11],
-                      [12, 14, 13, 15]], dtype=eval('torch.' + dtype)).npu()
-    output = torch.randint(1, (4,), dtype=eval('torch.' + dtype)).npu()
-    fn_npu_[ncore, 1, 1](output, x, XB, YB, ZB)
-    triton_ret = output[:, None] * 4 + x[None, :]
-    torch.testing.assert_close(triton_ret, a)
+    out0 = test_common.generate_tensor(shape, dtype).npu()
+    out1 = test_common.generate_tensor(shape, dtype).npu()
+    fn_npu_[1, 1, 1](out0, out1, shape[0], shape[1], size_g)
+    triton_ret = out0 * shape[0] + out1
+    torch.testing.assert_close(triton_ret, ans)
