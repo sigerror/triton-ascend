babb4675e6171fbbac994bc64923b78e18866ddc
Hall <qinhao61@h-partners.com>
2025-07-03 12:33:40 +0000

    fix(static range): fix assertion fail for static range dtype bf16
    Signed-off-by: Hall <qinhao61@h-partners.com>

--- ascend/examples/generalization_cases/test_range.py
+++ ascend/examples/generalization_cases/test_range.py
@@ -1,67 +1,169 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
 import pytest
-
 import triton
-import triton.language as tl
-import time
-
 import torch
-import torch_npu
 import test_common
+import logging
+
+import triton.language as tl
+from test_common import TestUtils
 
 
 @triton.jit
-def triton_add(in_ptr0, in_ptr1, out_ptr0, L : tl.constexpr, M : tl.constexpr, N : tl.constexpr):
-    lblk_idx = tl.arange(0,L)
-    mblk_idx = tl.arange(0,M)
-    nblk_idx = tl.arange(0,N)
-    idx = lblk_idx[:, None, None] * N * M + mblk_idx[None, :, None] * N + nblk_idx[None, None, :]
-    x0=tl.load(in_ptr0+idx)
-    x1=tl.load(in_ptr1+idx)
-    ret = x0 + x1
-    for i in tl.range(2,5,2):
-        ret = ret + x1
-    for i in tl.static_range(2,10,3):
-        ret = ret + x0
-    odx = lblk_idx[:, None, None] * N * M + mblk_idx[None, :, None] * N + nblk_idx[None, None, :]
-    tl.store(out_ptr0+odx, ret)
+def triton_range(output_ptr, x_ptr, y_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr,
+                 XNUMEL: tl.constexpr, YNUMEL: tl.constexpr, ZNUMEL: tl.constexpr):
+    xoffs = tl.program_id(0) * XB
+    yoffs = tl.program_id(1) * YB
+    zoffs = tl.program_id(2) * ZB
 
-testlist = [
-    (3,5,8),
-]
+    xidx = tl.arange(0, XB) + xoffs
+    yidx = tl.arange(0, YB) + yoffs
+    zidx = tl.arange(0, ZB) + zoffs
 
-def get_torch_typename(dtype):
-    if dtype == 'float32':
-        tyname = torch.float32
-    elif dtype == 'int32':
-        tyname = torch.int32
-    elif dtype == 'int64':
-        tyname = torch.int64
-    elif dtype == 'float16':
-        tyname = torch.float16
-    elif dtype == 'bfloat16':
-        tyname = torch.bfloat16
-    elif dtype == 'int16':
-        tyname = torch.int16
-    elif dtype == 'int8':
-        tyname = torch.int8
-    elif dtype == 'bool':
-        tyname = torch.bool
+    idx = xidx[:, None, None] * YNUMEL * ZNUMEL + yidx[None, :, None] * ZNUMEL + zidx[None, None, :]
+
+    X = tl.load(x_ptr + idx)
+    Y = tl.load(y_ptr + idx)
+
+    ret = X + Y
+    for _ in tl.range(2, 5, 2):
+        ret = ret + X
+
+    tl.store(output_ptr + idx, ret)
+
+
+@triton.jit
+def triton_static_range(output_ptr, x_ptr, y_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr,
+                        XNUMEL: tl.constexpr, YNUMEL: tl.constexpr, ZNUMEL: tl.constexpr):
+    xoffs = tl.program_id(0) * XB
+    yoffs = tl.program_id(1) * YB
+    zoffs = tl.program_id(2) * ZB
+
+    xidx = tl.arange(0, XB) + xoffs
+    yidx = tl.arange(0, YB) + yoffs
+    zidx = tl.arange(0, ZB) + zoffs
+
+    idx = xidx[:, None, None] * YNUMEL * ZNUMEL + yidx[None, :, None] * ZNUMEL + zidx[None, None, :]
+
+    X = tl.load(x_ptr + idx)
+    Y = tl.load(y_ptr + idx)
+
+    ret = X + Y
+    for _ in tl.static_range(2, 5, 2):
+        ret = ret + X
+
+    tl.store(output_ptr + idx, ret)
+
+
+test_shape = [(1,), (2,), (1, 1), (3, 4), (1, 1, 1), (2, 4, 8)]
+
+
+@pytest.mark.parametrize('shape', test_shape)
+@pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'float32', 'bfloat16'])
+def test_range(shape, dtype):
+    logging.log(logging.DEBUG, f"shape = {shape}")
+    x = test_common.generate_tensor(shape, dtype).npu()
+    y = test_common.generate_tensor(shape, dtype).npu()
+
+    new_shape = shape
+
+    output = torch.randint(1, new_shape, dtype=eval('torch.' + dtype)).npu()
+
+    logging.log(logging.DEBUG, f"output.dtype={output.dtype}")
+
+    if dtype == 'bfloat16':
+        ans = (x.to(torch.float32) + y.to(torch.float32) + x.to(torch.float32) + x.to(torch.float32)).to(torch.bfloat16)
     else:
-        raise ValueError('Invalid parameter \"dtype\" is found : {}'.format(dtype))
-    return tyname
+        ans = x + y + x + x
 
-typelist = ['int8','int16','int32','int64']
+    if len(shape) == 1:
+        XB = 1
+        xnumel = 1
+        YB = 1
+        ynumel = 1
+        ZB = shape[0]
+        znumel = shape[0]
+    elif len(shape) == 2:
+        XB = 1
+        xnumel = 1
+        YB = shape[0]
+        ynumel = shape[0]
+        ZB = shape[1]
+        znumel = shape[1]
+    else:
+        XB = shape[0]
+        xnumel = shape[0]
+        YB = shape[1]
+        ynumel = shape[1]
+        ZB = shape[2]
+        znumel = shape[2]
 
-@pytest.mark.parametrize('L, M, N',testlist)
-@pytest.mark.parametrize('sigtype',typelist)
-def test_add(sigtype, L, M, N):
-    dtype = get_torch_typename(sigtype)
-    shape = (L, M, N)
-    x0 = test_common.generate_tensor(shape = (L, M, N),dtype = sigtype).npu()
-    x1 = test_common.generate_tensor(shape = (L, M, N),dtype = sigtype).npu()
-    y_ref = x0 + x1 + x1 + x1 + x0 + x0 + x0
-    output = torch.zeros(shape, dtype=dtype).npu()
-    triton_add[1, 1, 1](x0, x1, output, L, M, N)
-    test_common.validate_cmp(sigtype, output, y_ref)
+    grid = (1, 1, 1)
+    if dtype == 'int8':
+        if x.numel() * x.element_size() >= 512:
+            grid = (1, 1, ZB)
+            ZB = 1
+    else:
+        if x.numel() * x.element_size() >= 8192:
+            grid = (1, 1, ZB)
+            ZB = 1
+
+    triton_range[grid](output, x, y, XB, YB, ZB, xnumel, ynumel, znumel)
+
+    test_common.validate_cmp(dtype, ans, output)
+
+
+@pytest.mark.parametrize('shape', test_shape)
+@pytest.mark.parametrize('dtype', ['int8', 'int16', 'int32', 'int64', 'float16', 'float32', 'bfloat16'])
+def test_static_range(shape, dtype):
+    logging.log(logging.DEBUG, f"shape = {shape}")
+    x = test_common.generate_tensor(shape, dtype).npu()
+    y = test_common.generate_tensor(shape, dtype).npu()
+
+    new_shape = shape
+
+    output = torch.randint(1, new_shape, dtype=eval('torch.' + dtype)).npu()
+
+    logging.log(logging.DEBUG, f"output.dtype={output.dtype}")
+
+    if dtype == 'bfloat16':
+        ans = (x.to(torch.float32) + y.to(torch.float32) + x.to(torch.float32) + x.to(torch.float32)).to(torch.bfloat16)
+    else:
+        ans = x + y + x + x
+
+    if len(shape) == 1:
+        XB = 1
+        xnumel = 1
+        YB = 1
+        ynumel = 1
+        ZB = shape[0]
+        znumel = shape[0]
+    elif len(shape) == 2:
+        XB = 1
+        xnumel = 1
+        YB = shape[0]
+        ynumel = shape[0]
+        ZB = shape[1]
+        znumel = shape[1]
+    else:
+        XB = shape[0]
+        xnumel = shape[0]
+        YB = shape[1]
+        ynumel = shape[1]
+        ZB = shape[2]
+        znumel = shape[2]
+
+    grid = (1, 1, 1)
+    if dtype == 'int8':
+        if x.numel() * x.element_size() >= 512:
+            grid = (1, 1, ZB)
+            ZB = 1
+    else:
+        if x.numel() * x.element_size() >= 8192:
+            grid = (1, 1, ZB)
+            ZB = 1
+
+    triton_static_range[grid](output, x, y, XB, YB, ZB, xnumel, ynumel, znumel)
+
+    test_common.validate_cmp(dtype, ans, output)
