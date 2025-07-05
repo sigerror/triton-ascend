73fad588b6c005daaf73c7f3fdcb2257e880519f
杨博楚 <yangbochu1@h-partners.com>
2025-07-05 11:09:10 +0800

    test(broadcast): add 2d, 3d, 4d, 5d broadcast testcase.

--- ascend/examples/generalization_cases/test_atomic_add.py
+++ ascend/examples/generalization_cases/test_atomic_add.py
@@ -196,3 +196,293 @@ def test_atomic_add_4d_5d(dtype, shape):
         triton_shape.append(1)
     atomic_add_multi_d[(1, )](x0, x1, *triton_shape)
     test_common.validate_cmp(dtype, x1, x1_ref)
+
+
+@triton.jit
+def atomic_add_broadcast_5d(x_ptr, y_ptr, out_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr, MB: tl.constexpr, NB: tl.constexpr,
+                            XB1: tl.constexpr, YB1: tl.constexpr, ZB1: tl.constexpr, MB1: tl.constexpr, NB1: tl.constexpr):
+    offsets = tl.arange(0, XB) * (YB * ZB * MB * NB)
+    offsets = offsets[:, None] + tl.arange(0, YB)[None, :] * (ZB * MB * NB)
+    offsets = offsets[:, :, None] + tl.arange(0, ZB)[None, None, :] * (MB * NB)
+    offsets = offsets[:, :, :, None] + tl.arange(0, MB)[None, None, None, :] * NB
+    offsets = offsets[:, :, :, :, None] + tl.arange(0, NB)[None, None, None, None, :]
+
+    offsets1 = tl.arange(0, XB1) * (YB1 * ZB1 * MB1 * NB1)
+    offsets1 = offsets1[:, None] + tl.arange(0, YB1)[None, :] * (ZB1 * MB1 * NB1)
+    offsets1 = offsets1[:, :, None] + tl.arange(0, ZB1)[None, None, :] * (MB1 * NB1)
+    offsets1 = offsets1[:, :, :, None] + tl.arange(0, MB1)[None, None, None, :] * NB1
+    offsets1 = offsets1[:, :, :, :, None] + tl.arange(0, NB1)[None, None, None, None, :]
+    
+    tmp0 = tl.load(x_ptr + offsets)
+    tmp1 = tl.load(y_ptr + offsets1)
+    tl.atomic_add(out_ptr + offsets1, tmp0)
+    tl.atomic_add(out_ptr + offsets1, tmp1)
+
+
+@pytest.mark.parametrize('param_list',
+    [
+        [(1, 1, 2, 1, 1), (1, 1, 2, 1, 2)],
+    ]
+    )
+@pytest.mark.parametrize('x_dtype_str', filtered_dtype)
+@pytest.mark.parametrize('y_dtype_str', filtered_dtype)
+def test_atomic_add_broadcast_5d(x_dtype_str, y_dtype_str, param_list):
+    x0_shape, y_shape = param_list
+
+    # 获取原始类型
+    x_dtype = eval('torch.' + x_dtype_str)
+    y_dtype = eval('torch.' + y_dtype_str)
+    if x_dtype == torch.int8 or x_dtype == torch.int16 or x_dtype == torch.int32:
+        x0 = torch.randint(low=0, high=100, size=x0_shape, dtype=x_dtype).npu()
+    else:
+        x0 = torch.randn(x0_shape, dtype=eval('torch.' + x_dtype_str)).npu()
+
+    if y_dtype == torch.int8 or y_dtype == torch.int16 or y_dtype == torch.int32:
+        y = torch.randint(low=0, high=100, size=y_shape, dtype=y_dtype).npu()
+    else:
+        y = torch.randn(y_shape, dtype=eval('torch.' + y_dtype_str)).npu()
+
+    out_dtype = promote_dtype(x_dtype, y_dtype)
+    if out_dtype == torch.bfloat16:
+        out_dtype = torch.float32
+    out = torch.full(y_shape, 0, dtype=out_dtype).npu()
+
+    x0_temp = x0.clone()
+    y_temp = y.clone()
+    out_temp = out.clone()
+
+    triton_shape = [*x0_shape]
+    while len(triton_shape) < 5:
+        triton_shape.append(1)
+    XB, YB, ZB, MB, NB = triton_shape
+
+    triton_shape1 = [*y_shape]
+    while len(triton_shape1) < 5:
+        triton_shape1.append(1)
+    XB1, YB1, ZB1, MB1, NB1 = triton_shape1
+
+    atomic_add_broadcast_5d[(1, )](
+        x_ptr=x0,
+        y_ptr=y, 
+        out_ptr=out, 
+        XB=XB, YB=YB, ZB=ZB, MB=MB, NB=NB,
+        XB1=XB1, YB1=YB1, ZB1=ZB1, MB1=MB1, NB1=NB1,
+        )
+    
+    expected = out_temp + y_temp + x0_temp
+    torch.testing.assert_close(out, expected)
+
+
+@triton.jit
+def atomic_add_broadcast_4d(x_ptr, y_ptr, out_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr, MB: tl.constexpr,
+                            XB1: tl.constexpr, YB1: tl.constexpr, ZB1: tl.constexpr, MB1: tl.constexpr):
+    offsets = tl.arange(0, XB) * (YB * ZB * MB)
+    offsets = offsets[:, None] + tl.arange(0, YB)[None, :] * (ZB * MB)
+    offsets = offsets[:, :, None] + tl.arange(0, ZB)[None, None, :] * (MB)
+    offsets = offsets[:, :, :, None] + tl.arange(0, MB)[None, None, None, :] 
+
+    offsets1 = tl.arange(0, XB1) * (YB1 * ZB1 * MB1)
+    offsets1 = offsets1[:, None] + tl.arange(0, YB1)[None, :] * (ZB1 * MB1)
+    offsets1 = offsets1[:, :, None] + tl.arange(0, ZB1)[None, None, :] * (MB1)
+    offsets1 = offsets1[:, :, :, None] + tl.arange(0, MB1)[None, None, None, :]
+    
+    tmp0 = tl.load(x_ptr + offsets)
+    tmp1 = tl.load(y_ptr + offsets1)
+    tl.atomic_add(out_ptr + offsets1, tmp0)
+    tl.atomic_add(out_ptr + offsets1, tmp1)
+
+
+@pytest.mark.parametrize('param_list',
+    [
+        [(1, 1, 2, 1), (1, 1, 2, 2)],
+        [(1, 1, 1, 1), (1, 1, 2, 2)],
+    ]
+    )
+@pytest.mark.parametrize('x_dtype_str', filtered_dtype)
+@pytest.mark.parametrize('y_dtype_str', filtered_dtype)
+def test_atomic_add_broadcast_4d(x_dtype_str, y_dtype_str, param_list):
+    x0_shape, y_shape = param_list
+
+    # 获取原始类型
+    x_dtype = eval('torch.' + x_dtype_str)
+    y_dtype = eval('torch.' + y_dtype_str)
+    if x_dtype == torch.int8 or x_dtype == torch.int16 or x_dtype == torch.int32:
+        x0 = torch.randint(low=0, high=100, size=x0_shape, dtype=x_dtype).npu()
+    else:
+        x0 = torch.randn(x0_shape, dtype=eval('torch.' + x_dtype_str)).npu()
+
+    if y_dtype == torch.int8 or y_dtype == torch.int16 or y_dtype == torch.int32:
+        y = torch.randint(low=0, high=100, size=y_shape, dtype=y_dtype).npu()
+    else:
+        y = torch.randn(y_shape, dtype=eval('torch.' + y_dtype_str)).npu()
+
+    out_dtype = promote_dtype(x_dtype, y_dtype)
+    if out_dtype == torch.bfloat16:
+        out_dtype = torch.float32
+    out = torch.full(y_shape, 0, dtype=out_dtype).npu()
+
+    x0_temp = x0.clone()
+    y_temp = y.clone()
+    out_temp = out.clone()
+
+    triton_shape = [*x0_shape]
+    while len(triton_shape) < 4:
+        triton_shape.append(1)
+    XB, YB, ZB, MB = triton_shape
+
+    triton_shape1 = [*y_shape]
+    while len(triton_shape1) < 4:
+        triton_shape1.append(1)
+    XB1, YB1, ZB1, MB1 = triton_shape1
+
+    atomic_add_broadcast_4d[(1, )](
+        x_ptr=x0,
+        y_ptr=y, 
+        out_ptr=out, 
+        XB=XB, YB=YB, ZB=ZB, MB=MB,
+        XB1=XB1, YB1=YB1, ZB1=ZB1, MB1=MB1,
+        )
+    
+    expected = out_temp + y_temp + x0_temp
+    torch.testing.assert_close(out, expected)
+
+
+@triton.jit
+def atomic_add_broadcast_3d(x_ptr, y_ptr, out_ptr, XB: tl.constexpr, YB: tl.constexpr, ZB: tl.constexpr,
+                            XB1: tl.constexpr, YB1: tl.constexpr, ZB1: tl.constexpr):
+    offsets = tl.arange(0, XB) * (YB * ZB)
+    offsets = offsets[:, None] + tl.arange(0, YB)[None, :] * (ZB)
+    offsets = offsets[:, :, None] + tl.arange(0, ZB)[None, None, :]
+
+    offsets1 = tl.arange(0, XB1) * (YB1 * ZB1)
+    offsets1 = offsets1[:, None] + tl.arange(0, YB1)[None, :] * (ZB1)
+    offsets1 = offsets1[:, :, None] + tl.arange(0, ZB1)[None, None, :]
+    
+    tmp0 = tl.load(x_ptr + offsets)
+    tmp1 = tl.load(y_ptr + offsets1)
+    tl.atomic_add(out_ptr + offsets1, tmp0)
+    tl.atomic_add(out_ptr + offsets1, tmp1)
+
+
+@pytest.mark.parametrize('param_list',
+    [
+        [(1, 1, 2), (1, 2, 2)],
+    ]
+    )
+@pytest.mark.parametrize('x_dtype_str', filtered_dtype)
+@pytest.mark.parametrize('y_dtype_str', filtered_dtype)
+def test_atomic_add_broadcast_3d(x_dtype_str, y_dtype_str, param_list):
+    x0_shape, y_shape = param_list
+
+    # 获取原始类型
+    x_dtype = eval('torch.' + x_dtype_str)
+    y_dtype = eval('torch.' + y_dtype_str)
+    if x_dtype == torch.int8 or x_dtype == torch.int16 or x_dtype == torch.int32:
+        x0 = torch.randint(low=0, high=100, size=x0_shape, dtype=x_dtype).npu()
+    else:
+        x0 = torch.randn(x0_shape, dtype=eval('torch.' + x_dtype_str)).npu()
+
+    if y_dtype == torch.int8 or y_dtype == torch.int16 or y_dtype == torch.int32:
+        y = torch.randint(low=0, high=100, size=y_shape, dtype=y_dtype).npu()
+    else:
+        y = torch.randn(y_shape, dtype=eval('torch.' + y_dtype_str)).npu()
+
+    out_dtype = promote_dtype(x_dtype, y_dtype)
+    if out_dtype == torch.bfloat16:
+        out_dtype = torch.float32
+    out = torch.full(y_shape, 0, dtype=out_dtype).npu()
+
+    x0_temp = x0.clone()
+    y_temp = y.clone()
+    out_temp = out.clone()
+
+    triton_shape = [*x0_shape]
+    while len(triton_shape) < 3:
+        triton_shape.append(1)
+    XB, YB, ZB = triton_shape
+
+    triton_shape1 = [*y_shape]
+    while len(triton_shape1) < 3:
+        triton_shape1.append(1)
+    XB1, YB1, ZB1 = triton_shape1
+
+    atomic_add_broadcast_3d[(1, )](
+        x_ptr=x0,
+        y_ptr=y, 
+        out_ptr=out, 
+        XB=XB, YB=YB, ZB=ZB,
+        XB1=XB1, YB1=YB1, ZB1=ZB1,
+        )
+    
+    expected = out_temp + y_temp + x0_temp
+    torch.testing.assert_close(out, expected)
+
+
+@triton.jit
+def atomic_add_broadcast_2d(x_ptr, y_ptr, out_ptr, XB: tl.constexpr, YB: tl.constexpr,
+                            XB1: tl.constexpr, YB1: tl.constexpr):
+    offsets = tl.arange(0, XB) * (YB)
+    offsets = offsets[:, None] + tl.arange(0, YB)[None, :]
+
+    offsets1 = tl.arange(0, XB1) * (YB1)
+    offsets1 = offsets1[:, None] + tl.arange(0, YB1)[None, :]
+    
+    tmp0 = tl.load(x_ptr + offsets)
+    tmp1 = tl.load(y_ptr + offsets1)
+    tl.atomic_add(out_ptr + offsets1, tmp0)
+    tl.atomic_add(out_ptr + offsets1, tmp1)
+
+
+@pytest.mark.parametrize('param_list',
+    [
+        [(1, 2), (2, 2)],
+         [(1, 1), (2, 2)],
+    ]
+    )
+@pytest.mark.parametrize('x_dtype_str', filtered_dtype)
+@pytest.mark.parametrize('y_dtype_str', filtered_dtype)
+def test_atomic_add_broadcast_2d(x_dtype_str, y_dtype_str, param_list):
+    x0_shape, y_shape = param_list
+
+    # 获取原始类型
+    x_dtype = eval('torch.' + x_dtype_str)
+    y_dtype = eval('torch.' + y_dtype_str)
+    if x_dtype == torch.int8 or x_dtype == torch.int16 or x_dtype == torch.int32:
+        x0 = torch.randint(low=0, high=100, size=x0_shape, dtype=x_dtype).npu()
+    else:
+        x0 = torch.randn(x0_shape, dtype=eval('torch.' + x_dtype_str)).npu()
+
+    if y_dtype == torch.int8 or y_dtype == torch.int16 or y_dtype == torch.int32:
+        y = torch.randint(low=0, high=100, size=y_shape, dtype=y_dtype).npu()
+    else:
+        y = torch.randn(y_shape, dtype=eval('torch.' + y_dtype_str)).npu()
+
+    out_dtype = promote_dtype(x_dtype, y_dtype)
+    if out_dtype == torch.bfloat16:
+        out_dtype = torch.float32
+    out = torch.full(y_shape, 0, dtype=out_dtype).npu()
+
+    x0_temp = x0.clone()
+    y_temp = y.clone()
+    out_temp = out.clone()
+
+    triton_shape = [*x0_shape]
+    while len(triton_shape) < 2:
+        triton_shape.append(1)
+    XB, YB = triton_shape
+
+    triton_shape1 = [*y_shape]
+    while len(triton_shape1) < 2:
+        triton_shape1.append(1)
+    XB1, YB1 = triton_shape1
+
+    atomic_add_broadcast_2d[(1, )](
+        x_ptr=x0,
+        y_ptr=y, 
+        out_ptr=out, 
+        XB=XB, YB=YB,
+        XB1=XB1, YB1=YB1,
+        )
+    
+    expected = out_temp + y_temp + x0_temp
+    torch.testing.assert_close(out, expected)
