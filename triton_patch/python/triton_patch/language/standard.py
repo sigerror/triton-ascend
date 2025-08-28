60086d1ed8c2b3b7690cd6c0859c8bb837438d29
candyhong <1102229410@qq.com>
2025-08-28 08:03:41 +0000

    fix(op): correct CompilationError message for bool(int8 or int1)

--- triton_patch/python/triton_patch/language/standard.py
+++ triton_patch/python/triton_patch/language/standard.py
@@ -24,6 +24,8 @@ def flip(x, dim=None):
 @jit
 @math._add_math_1arg_docstr("sigmoid")
 def sigmoid(x):
+    _is_int8_type: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
     _is_floating_type: core.constexpr = x.dtype.is_floating()
     core.static_assert(_is_floating_type == True, f"Expected dtype fp16/fp32/bf16, but got {core.constexpr(x.dtype)}")
     return (1 / (1 + math.exp(-x.to(core.float32)))).to(x.dtype)
@@ -32,6 +34,8 @@ def sigmoid(x):
 @jit
 @math._add_math_1arg_docstr("softmax")
 def softmax(x, ieee_rounding=False):
+    _is_int8_type: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
     _is_floating_type: core.constexpr = x.dtype.is_floating()
     core.static_assert(_is_floating_type == True, f"Expected dtype fp16/fp32/bf16, but got {core.constexpr(x.dtype)}")
     z = x.to(core.float32) - max(x, 0)
@@ -43,6 +47,8 @@ def softmax(x, ieee_rounding=False):
 @jit
 @math._add_math_1arg_docstr("isfinited")
 def isfinited(x):
+    _is_int8_type: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
     _is_floating_type: core.constexpr = x.dtype.is_floating()
     core.static_assert(_is_floating_type == True, f"Expected dtype fp16/fp32/bf16, but got {core.constexpr(x.dtype)}")
     nan_mask = math.isnan(x)
@@ -53,6 +59,8 @@ def isfinited(x):
 @jit
 @math._add_math_1arg_docstr("finitef")
 def finitef(x):
+    _is_int8_type: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type, f"finitef only supports float32, but got int8 or int1")
     core.static_assert(x.dtype == float32, f"finitef only supports float32, but got {core.constexpr(x.dtype)}")
     nan_mask = math.isnan(x)
     inf_mask = math.isinf(x)
@@ -62,6 +70,8 @@ def finitef(x):
 @jit
 @math._add_math_1arg_docstr("rint")
 def rint(x):
+    _is_int8_type: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
     _is_floating_type: core.constexpr = x.dtype.is_floating()
     core.static_assert(_is_floating_type == True, f"Expected dtype fp16/fp32/bf16, but got {core.constexpr(x.dtype)}")
     return core.where(x >= 0, math.floor(x + 0.5), math.ceil(x - 0.5))
@@ -72,6 +82,10 @@ pi: core.constexpr = math_pi
 @jit
 @math._add_math_2arg_docstr("atan2")
 def atan2(y, x):
+    _is_int8_type_x: core.constexpr = x.dtype.is_int8()
+    core.static_assert(not _is_int8_type_x, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
+    _is_int8_type_y: core.constexpr = y.dtype.is_int8()
+    core.static_assert(not _is_int8_type_y, f"Expected dtype fp16/fp32/bf16, but got int8 or int1")
     _is_floating_type_x: core.constexpr = x.dtype.is_floating()
     core.static_assert(_is_floating_type_x == True, f"Expected dtype fp16/fp32/bf16, but got {core.constexpr(x.dtype)}")
     _is_floating_type_y: core.constexpr = y.dtype.is_floating()
