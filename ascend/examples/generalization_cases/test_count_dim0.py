test(op): fix count sum ub overflow error

ascend/examples/generalization_cases/test_count_dim0.py
@@ -23,13 +23,13 @@ def standard_count_lt(x0, cmp_val, dim, dtype):
 
 @triton.jit
 def count(in_ptr0, out_ptr0, cmp_val, dim : tl.constexpr, M : tl.constexpr, N : tl.constexpr, MNUMEL: tl.constexpr, NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0,MNUMEL)
-    nblk_idx = tl.arange(0,NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
-    mask = (mmask[:,None]) & (nmask[None,:])
-    idx = mblk_idx[:,None]*N + nblk_idx[None,:]
-    x = tl.load(in_ptr0+idx, mask = mask, other = 0)
+    mblk_idx = tl.arange(0, MNUMEL)
+    nblk_idx = tl.arange(0, N) + tl.program_id(2) * N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
+    mask = (mmask[:, None]) & (nmask[None, :])
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
+    x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x == cmp_val)
     tmp2 = tmp1.to(tl.float32)
     ret = tl.sum(tmp2, dim)
@@ -37,13 +37,13 @@ def count(in_ptr0, out_ptr0, cmp_val, dim : tl.constexpr, M : tl.constexpr, N : 
 
 @triton.jit
 def count_gt(in_ptr0, out_ptr0, cmp_val, dim : tl.constexpr, M : tl.constexpr, N : tl.constexpr, MNUMEL: tl.constexpr, NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0,MNUMEL)
-    nblk_idx = tl.arange(0,NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
-    mask = (mmask[:,None]) & (nmask[None,:])
-    idx = mblk_idx[:,None]*N + nblk_idx[None,:]
-    x = tl.load(in_ptr0+idx, mask = mask, other = 0)
+    mblk_idx = tl.arange(0, MNUMEL)
+    nblk_idx = tl.arange(0, N) + tl.program_id(2) * N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
+    mask = (mmask[:, None]) & (nmask[None, :])
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
+    x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x > cmp_val)
     tmp2 = tmp1.to(tl.float32)
     ret = tl.sum(tmp2, dim)
@@ -51,13 +51,13 @@ def count_gt(in_ptr0, out_ptr0, cmp_val, dim : tl.constexpr, M : tl.constexpr, N
 
 @triton.jit
 def count_lt(in_ptr0, out_ptr0, cmp_val, dim : tl.constexpr, M : tl.constexpr, N : tl.constexpr, MNUMEL: tl.constexpr, NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0,MNUMEL)
-    nblk_idx = tl.arange(0,NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
-    mask = (mmask[:,None]) & (nmask[None,:])
-    idx = mblk_idx[:,None]*N + nblk_idx[None,:]
-    x = tl.load(in_ptr0+idx, mask = mask, other = 0)
+    mblk_idx = tl.arange(0, MNUMEL)
+    nblk_idx = tl.arange(0, N) + tl.program_id(2) * N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
+    mask = (mmask[:, None]) & (nmask[None, :])
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
+    x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x < cmp_val)
     tmp2 = tmp1.to(tl.float32)
     ret = tl.sum(tmp2, dim)
@@ -75,10 +75,10 @@ def test_count_dim0_common(shape, dtype):
     else:
         cmp_val = 0.5
 
-    ans = standard_count(x0, cmp_val,0, dtype)
+    ans = standard_count(x0, cmp_val, 0, dtype)
 
     output = torch.zeros((shape[1],), dtype = torch.float32).npu()
-    count[1,1,1](x0, output, cmp_val, 0, xblock, rblock, xblock, rblock)
+    count[1, 1, rblock](x0, output, cmp_val, 0, xblock, 1, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))
 
@@ -97,7 +97,7 @@ def test_count_gt_dim0_common(shape, dtype):
     ans = standard_count_gt(x0, cmp_val,0, dtype)
 
     output = torch.zeros((shape[1],), dtype = torch.float32).npu()
-    count_gt[1,1,1](x0, output, cmp_val, 0, xblock, rblock, xblock, rblock)
+    count_gt[1, 1, rblock](x0, output, cmp_val, 0, xblock, 1, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))
 
@@ -116,6 +116,6 @@ def test_count_lt_dim0_common(shape, dtype):
     ans = standard_count_lt(x0, cmp_val,0, dtype)
 
     output = torch.zeros((shape[1],), dtype = torch.float32).npu()
-    count_lt[1,1,1](x0, output, cmp_val, 0, xblock, rblock, xblock, rblock)
+    count_lt[1, 1, rblock](x0, output, cmp_val, 0, xblock, 1, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))

ascend/examples/generalization_cases/test_count_dim1.py
@@ -28,12 +28,12 @@ def standard_count_lt(x0, cmp_val, dim, dtype):
 @triton.jit
 def count(in_ptr0, out_ptr0, cmp_val, dim: tl.constexpr, M: tl.constexpr, N: tl.constexpr, MNUMEL: tl.constexpr,
           NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0, MNUMEL)
+    mblk_idx = tl.arange(0, M) + tl.program_id(1) * M
     nblk_idx = tl.arange(0, NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
     mask = (mmask[:, None]) & (nmask[None, :])
-    idx = mblk_idx[:, None] * N + nblk_idx[None, :]
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
     x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x == cmp_val)
     tmp2 = tmp1.to(tl.float32)
@@ -44,12 +44,12 @@ def count(in_ptr0, out_ptr0, cmp_val, dim: tl.constexpr, M: tl.constexpr, N: tl.
 @triton.jit
 def count_gt(in_ptr0, out_ptr0, cmp_val, dim: tl.constexpr, M: tl.constexpr, N: tl.constexpr, MNUMEL: tl.constexpr,
              NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0, MNUMEL)
+    mblk_idx = tl.arange(0, M) + tl.program_id(1) * M
     nblk_idx = tl.arange(0, NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
     mask = (mmask[:, None]) & (nmask[None, :])
-    idx = mblk_idx[:, None] * N + nblk_idx[None, :]
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
     x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x > cmp_val)
     tmp2 = tmp1.to(tl.float32)
@@ -60,12 +60,12 @@ def count_gt(in_ptr0, out_ptr0, cmp_val, dim: tl.constexpr, M: tl.constexpr, N: 
 @triton.jit
 def count_lt(in_ptr0, out_ptr0, cmp_val, dim: tl.constexpr, M: tl.constexpr, N: tl.constexpr, MNUMEL: tl.constexpr,
              NNUMEL: tl.constexpr):
-    mblk_idx = tl.arange(0, MNUMEL)
+    mblk_idx = tl.arange(0, M) + tl.program_id(1) * M
     nblk_idx = tl.arange(0, NNUMEL)
-    mmask = mblk_idx < M
-    nmask = nblk_idx < N
+    mmask = mblk_idx < MNUMEL
+    nmask = nblk_idx < NNUMEL
     mask = (mmask[:, None]) & (nmask[None, :])
-    idx = mblk_idx[:, None] * N + nblk_idx[None, :]
+    idx = mblk_idx[:, None] * NNUMEL + nblk_idx[None, :]
     x = tl.load(in_ptr0 + idx, mask=mask, other=0)
     tmp1 = (x < cmp_val)
     tmp2 = tmp1.to(tl.float32)
@@ -87,7 +87,7 @@ def test_count_dim1_common(shape, dtype):
     ans = standard_count(x0, cmp_val,1, dtype)
 
     output = torch.zeros((shape[0],), dtype = torch.float32).npu()
-    count[1,1,1](x0, output, cmp_val, 1, xblock, rblock, xblock, rblock)
+    count[1, xblock, 1](x0, output, cmp_val, 1, 1, rblock, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))
 
@@ -106,7 +106,7 @@ def test_count_gt_dim1_common(shape, dtype):
     ans = standard_count_gt(x0, cmp_val,1, dtype)
 
     output = torch.zeros((shape[0],), dtype = torch.float32).npu()
-    count_gt[1,1,1](x0, output, cmp_val, 1, xblock, rblock, xblock, rblock)
+    count_gt[1, xblock, 1](x0, output, cmp_val, 1, 1, rblock, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))
 
@@ -125,6 +125,6 @@ def test_count_lt_dim1_common(shape, dtype):
     ans = standard_count_lt(x0, cmp_val,1, dtype)
 
     output = torch.zeros((shape[0],), dtype = torch.float32).npu()
-    count_lt[1,1,1](x0, output, cmp_val, 1, xblock, rblock, xblock, rblock)
+    count_lt[1, xblock, 1](x0, output, cmp_val, 1, 1, rblock, xblock, rblock)
 
     test_common.validate_cmp("float32", output, ans.to(torch.float32))