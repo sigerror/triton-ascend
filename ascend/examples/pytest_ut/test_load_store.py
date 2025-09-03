275a94c58d1b63bf778e88b5f76047a8227bbf36
candyhong <1102229410@qq.com>
2025-09-03 01:27:45 +0000

    test(op): add mask with <= case in load/store kernel

--- ascend/examples/pytest_ut/test_load_store.py
+++ ascend/examples/pytest_ut/test_load_store.py
@@ -47,6 +47,17 @@ def triton_load_store_multi_d(
     tl.store(out_ptr0 + offsets, tmp_out, masks)
 
 
+@triton.jit
+def triton_load_store_sle_mask(in_ptr0, out_ptr0, xnumel, XBLOCK: tl.constexpr, XBLOCK_SUB: tl.constexpr):
+    xoffset = tl.program_id(0) * XBLOCK
+    for xoffset_sub in range(0, XBLOCK, XBLOCK_SUB):
+        x_index = xoffset + xoffset_sub + tl.arange(0, XBLOCK_SUB)[:]
+        xmask = x_index <= xnumel
+        tmp0 = tl.load(in_ptr0 + x_index, xmask)
+        tmp2 = tmp0
+        tl.store(out_ptr0 + x_index, tmp2, xmask)
+
+
 @pytest.mark.parametrize('param_list',
                          [
                              ['float32', (2, 4096, 8), 2, 32768, 1024],
@@ -102,3 +113,26 @@ def test_load_store_multi_d(param_list):
     triton_load_store_multi_d[(1, )](x0, y_actual, *blocks, *blocks, *shapes)
     # 比较结果
     test_common.validate_cmp(dtype, y_actual, y_expect)
+
+
+@pytest.mark.parametrize('param_list',
+                         [
+                             ['float32', (2, 4096, 8), 2, 32768, 1024],
+                             ['float16', (2, 4096, 8), 2, 32768, 1024],
+                             ['int8', (2, 4096, 8), 2, 32768, 1024],
+                             ['float32', (8, 8, 4), 2, 128, 64],
+                             ['float16', (8, 8, 4), 2, 128, 64],
+                             ['int8', (8, 8, 4), 2, 128, 64],
+                         ]
+                         )
+def test_load_store_sle_mask(param_list):
+    # 生成数据
+    dtype, shape, ncore, xblock, xblock_sub = param_list
+    x0 = test_common.generate_tensor(shape, dtype).npu()
+    # torch结果
+    y_ref = x0
+    # triton结果
+    y_cal = test_common.generate_tensor(shape, dtype).npu()
+    triton_load_store_sle_mask[(ncore, )](x0, y_cal, x0.numel() - 1, xblock, xblock_sub)
+    # 比较结果
+    test_common.validate_cmp(dtype, y_cal, y_ref)
