1fee671bd66afc0bd41505631c763a23745af1ee
lu2048 <luwei789@huawei.com>
2025-09-25 15:31:49 +0800

    test(sglang): add UT cases for sglang kernel(moe_align_block_size_stage3)

---
+++ ascend/test/sglang/v0.4.8/test_moe_align_block_size_stage3.py
@@ -0,0 +1,46 @@
+import sys
+import pytest
+import triton
+import torch
+import triton.language as tl
+
+sys.path.append("..")
+import test_common
+
+
+# source: 
+#   sgl-kernel\tests\test_moe_align.py
+#   sgl-kernel\benchmark\bench_moe_align_block_size.py
+#   python\sglang\srt\layers\moe\fused_moe_triton\fused_moe.py
+@triton.jit
+def moe_align_block_size_stage3(
+    total_tokens_post_pad_ptr,
+    tokens_cnts_ptr,
+    cumsum_ptr,
+    num_experts: tl.constexpr,
+    block_size: tl.constexpr,
+):
+    last_cumsum = 0
+    off_cnt = num_experts * num_experts
+    for i in range(1, num_experts + 1):
+        token_cnt = tl.load(tokens_cnts_ptr + off_cnt + i - 1)
+        last_cumsum = last_cumsum + tl.cdiv(token_cnt, block_size) * block_size
+        tl.store(cumsum_ptr + i, last_cumsum)
+    tl.store(total_tokens_post_pad_ptr, last_cumsum)
+
+
+def test_moe_align_block_size_stage3(ptfile_path):
+    try:
+        data = torch.load(ptfile_path, map_location=torch.device('cpu'), weights_only=False)
+    except Exception as e:
+        pytest.fail(f"load file {ptfile_path} failed: {str(e)}")
+
+    input_data = test_common.convert_tensor_with_device_type(data["input_data"], device_type='npu')
+
+    moe_align_block_size_stage3[data["grid"]](**input_data)
+
+    # compare the results of GPU and NPU.
+    try:
+        test_common.compare_data_precision(data["gpu_output"], input_data, device_type='cpu')
+    except ValueError as e:
+        pytest.fail(f"The testcase failed")
